d7278831d6e7e7b43b7e36173b98b3b23ea7c3da
Josu <josepadillarivas251004@gmail.com>
2024-07-26 08:13:39 -0600

    Actualizar perfil y cambio de interfaz
    Aporte de jose padilla

---
+++ proyectoGrupo#1/proyectoGrupo#1/Controllers/PerfilController.cs
@@ -0,0 +1,36 @@
+﻿using proyectoGrupo_1.Entidades;
+using proyectoGrupo_1.Models;
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace KN_Web.Controllers
+{
+    [Filtro]
+    [OutputCache(NoStore = true, VaryByParam = "*", Duration = 0)]
+    public class PerfilController : Controller
+    {
+        UsuarioModel usuarioM = new UsuarioModel();
+
+        [HttpGet]
+        public ActionResult Perfil()
+        {
+            var respuesta = usuarioM.ConsultarUsuario(int.Parse(Session["idUsuario"].ToString()));
+            return View(respuesta);
+        }
+
+        [HttpPost]
+        public ActionResult ActualizarPerfil(Usuario user)
+        {
+            var respuesta = usuarioM.ActualizarUsuario(user);
+
+            if (respuesta)
+                return RedirectToAction("Perfil", "Perfil");
+            else
+            {
+                ViewBag.msj = "No se ha podido actualizar su información de perfil";
+                return View();
+            }
+        }
+    }
+}
